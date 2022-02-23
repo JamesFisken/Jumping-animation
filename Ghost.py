29728b192f079b34bfe14ae00262d9b344ebbd15
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2022-02-24 09:33:46 +1300

    i made a jumping animation

---
+++ Ghost.py
@@ -0,0 +1,17 @@
+import os
+import time
+fps = 5
+while True:
+	time.sleep(1/fps)
+	os.system('cls')
+	print(" O ", "        i'm doing jumping jacks")
+	print("\|/")
+	print(" | ")
+	print("/ \ ")
+	time.sleep(1/fps)
+	os.system('cls')
+	print("   ", "        i'm doing jumping jacks")
+	print(" O ")
+	print("|||")
+	print(" | ")
+	print("| | ")
